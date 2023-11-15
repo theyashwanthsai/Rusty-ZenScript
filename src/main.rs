a1df4fcf5a27f770f42affb5b692ab0586c83749
Sai Yashwanth <taddishetty34@gmail.com>
2023-11-15 23:24:20 +0530

    Init

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use std::io::{stdin};
 
 
-
+fn lexer(string: String){
+    println!("Todo");
+}
 
 fn main(){
-    while true{
-        println!("ZenScript>")
+    let flag = true;
+    while flag{
+        println!("ZenScript>");
         let mut input = String::new();
-        stdin.read_line(&mut input);
+        stdin().read_line(&mut input);
         // Todo: Implement a Lexer which will scan and tokenize the input string.
-        //lexer(input);
+        lexer(input);
     }
 }
---
+++ target/debug/Rusty-ZenScript.d
@@ -0,0 +1,1 @@
+/Users/saiyashwanth/Dev/Rust/Rusty-ZenScript/target/debug/Rusty-ZenScript: /Users/saiyashwanth/Dev/Rust/Rusty-ZenScript/src/main.rs
