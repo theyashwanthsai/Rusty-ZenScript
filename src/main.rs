9f93bb27351af63de8f3538c033396b9ded0e551
Sai Yashwanth <taddishetty34@gmail.com>
2023-11-15 23:36:26 +0530

    pretty input

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ use std::io::{stdin};
 
 
 fn lexer(string: String){
-    println!("Todo");
+    println!("{}", string);
 }
 
 fn main(){
     let flag = true;
     while flag{
-        println!("ZenScript>");
+        println!("ZenScript> ");
         let mut input = String::new();
         stdin().read_line(&mut input);
+        let input = input.trim().to_string();
         // Todo: Implement a Lexer which will scan and tokenize the input string.
         lexer(input);
     }
