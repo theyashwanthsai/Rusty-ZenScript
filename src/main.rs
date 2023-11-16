ef2f6a677f6ff9d3da6f75910f54b5165d0b0100
Sai Yashwanth <taddishetty34@gmail.com>
2023-11-16 15:11:34 +0530

    lexer

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,72 @@
 use std::io::{stdin};
+use std::str;
+// use std::vec;
 
 
-fn lexer(string: String){
-    println!("{}", string);
+
+//////////////////////////////////////////////////////////////////////
+//                             Constants                            //
+//////////////////////////////////////////////////////////////////////
+static  DIGITS: &str = "0123456789";
+
+
+
+//////////////////////////////////////////////////////////////////////
+//                             Tokens                               //
+//////////////////////////////////////////////////////////////////////
+#[derive(Debug)]
+enum Tokens{
+    NUM,
+    PLUS,
+    MINUS,
+    DIV,
+    MUL
 }
+
+
+
+
+//////////////////////////////////////////////////////////////////////
+//                             Lexer                                //
+//////////////////////////////////////////////////////////////////////
+
+
+fn lexer(source: String){
+    // println!("{}", string);
+    let mut token: Vec<Tokens> = Vec::new();
+    for c in source.chars(){
+        if DIGITS.contains(c){
+            token.push(Tokens::NUM);
+        }
+        if c == '+'{
+            token.push(Tokens::PLUS);
+        }
+
+
+
+
+
+
+    }
+    // println!("{:?}", token);
+    println!("{:?}", token);
+
+}
+
+
+
+
 
 fn main(){
     let flag = true;
     while flag{
         println!("ZenScript> ");
         let mut input = String::new();
-        stdin().read_line(&mut input);
+        let _ = stdin().read_line(&mut input);
         let input = input.trim().to_string();
         // Todo: Implement a Lexer which will scan and tokenize the input string.
         lexer(input);
     }
 }
+
+
