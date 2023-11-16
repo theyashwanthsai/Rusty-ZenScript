a006649a4d4acb0359559181d0bc14a42c323e8c
Sai Yashwanth <taddishetty34@gmail.com>
2023-11-16 11:51:14 +0530

    gitignore

---
+++ target/debug/deps/Rusty_ZenScript-3cc2d9c756590b47.d
@@ -0,0 +1,5 @@
+/Users/saiyashwanth/Dev/Rust/Rusty-ZenScript/target/debug/deps/Rusty_ZenScript-3cc2d9c756590b47.rmeta: src/main.rs
+
+/Users/saiyashwanth/Dev/Rust/Rusty-ZenScript/target/debug/deps/Rusty_ZenScript-3cc2d9c756590b47.d: src/main.rs
+
+src/main.rs:
